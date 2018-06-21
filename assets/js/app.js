1f733fe2a4dc96f6814505906df08211b37887f6
Andreas Halberkamp <>
2018-06-21 09:29:59 +0200

    Add jquery and bootstrap.

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+require('../css/app.css')
+
+// loads the jquery package from node_modules
+const $ = require('jquery');
+require('bootstrap-sass');
+$(document).ready(function() {
+    var body=$('body');
+alert(body);
+});
