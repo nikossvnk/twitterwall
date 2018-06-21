9fed9f34b979bd62dc7a187f09ba0ebaccb24c1b
Andreas Halberkamp <>
2018-06-21 14:08:05 +0200

    Add handlebars.

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ require('../css/global.scss')
 // loads the jquery package from node_modules
 const $ = require('jquery');
 require('bootstrap-sass');
+require('../js/handlebars.js');
 $(document).ready(function() {
     var body=$('body');
 
